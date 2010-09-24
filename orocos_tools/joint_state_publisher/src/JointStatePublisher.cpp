9d5e4c5f874b332dc9be7cb92d01bebd5c68bf4a
Konrad Banachowicz <konradb3@gmail.com>
2010-09-24 14:31:57 +0200

    joint_spline_trajectory_generator : added. oro_joint_trajectory_action : added.

--- orocos_tools/joint_state_publisher/src/JointStatePublisher.cpp
+++ orocos_tools/joint_state_publisher/src/JointStatePublisher.cpp
@@ -1,62 +1,60 @@
-
 #include <rtt/os/TimeService.hpp>
 #include <rtt/Time.hpp>
 #include <ocl/Component.hpp>
 
 #include "JointStatePublisher.hpp"
 
-JointStatePublisher::JointStatePublisher(const std::string& name) : RTT::TaskContext(name, PreOperational), msrJnt_port("msrJnt"), jointState_port("jointState"), nJoints_prop("nJoints", "number of joints"), jointNames_prop("jointNames", "names of joints")
-{
+JointStatePublisher::JointStatePublisher(const std::string& name) :
+	RTT::TaskContext(name, PreOperational), msrJnt_port("msrJnt"),
+			jointState_port("jointState"), nJoints_prop("numberOfJoints",
+					"number of joints") {
 	ports()->addPort(msrJnt_port);
 	ports()->addPort(jointState_port);
 
 	this->addProperty(nJoints_prop);
-	this->addProperty(jointNames_prop);
 }
 
-JointStatePublisher::~JointStatePublisher()
-{
+JointStatePublisher::~JointStatePublisher() {
 }
 
-bool JointStatePublisher::configureHook()
-{
+bool JointStatePublisher::configureHook() {
 	nJoints = nJoints_prop.get();
-//	std::vector<std::string> names;// = jointNames_prop.get();
-	
+
 	names.resize(nJoints);
 
-	for(int i =0; i<nJoints; i++)
-	{
-		names[i] = ((RTT::Property<std::string>*)this->getProperty(std::string("joint")+(char)(i+48)+"_name"))->get();
+	for (int i = 0; i < nJoints; i++) {
+		names[i] = ((RTT::Property<std::string>*) this->getProperty(
+				std::string("joint") + (char) (i + 48) + "_name"))->get();
 	}
-	if(nJoints != names.size())
-	{
+	if (nJoints != names.size()) {
 		return false;
-}
-	
+	}
+
 	jState.name.resize(nJoints);
 	jState.position.resize(nJoints);
+	jState.velocity.resize(nJoints);
 
-	for(int i = 0; i < nJoints; i++)
-	{
+	for (int i = 0; i < nJoints; i++) {
 		jState.name[i] = names[i].c_str();
 	}
 
 	return true;
 }
 
-void JointStatePublisher::updateHook()
-{
-	if(msrJnt_port.read(msrJnt) == RTT::NewData)
-	{
-		if(msrJnt.size() == nJoints)
-		{
-			RTT::os::TimeService::nsecs n = RTT::os::TimeService::Instance()->getNSecs();
-			jState.header.stamp.sec = n/1000000000;
-			jState.header.stamp.nsec = n%1000000000;
-			for(int i = 0; i < nJoints; i++)
-				jState.position[i] = msrJnt[i];
+void JointStatePublisher::updateHook() {
+	if (msrJnt_port.read(msrJnt) == RTT::NewData) {
+		if (msrJnt.size() == nJoints) {
+			jState.header.stamp = ros::Time::now();
+			for (int i = 0; i < nJoints; i++) {
+				jState.position[i] = msrJnt[i].position;
+				jState.velocity[i] = msrJnt[i].velocity;
+			}
 			jointState_port.write(jState);
+		} else {
+			RTT::Logger::log(RTT::Logger::Error)
+					<< "Received joint state have invalid size (received : "
+					<< msrJnt.size() << " expected : " << nJoints << " )"
+					<< RTT::endlog();
 		}
 	}
 }
--- orocos_tools/joint_state_publisher/src/JointStatePublisher.hpp
+++ orocos_tools/joint_state_publisher/src/JointStatePublisher.hpp
@@ -9,24 +9,24 @@
 #include <rtt/Property.hpp>
 
 #include "sensor_msgs/JointState.h"
+#include "JointState.hpp"
 
 class JointStatePublisher : public RTT::TaskContext
 {
 	public:
 		JointStatePublisher(const std::string& name);
 		~JointStatePublisher();
-		
+
 		bool configureHook();
 		void updateHook();
 	protected:
-		RTT::InputPort<std::vector<double> > msrJnt_port;
+		RTT::InputPort<std::vector<JointState> > msrJnt_port;
 		RTT::OutputPort<sensor_msgs::JointState> jointState_port;
 
 		RTT::Property<int> nJoints_prop;
-		RTT::Property<std::vector<std::string> > jointNames_prop;
 	private:
 		sensor_msgs::JointState jState;
-		std::vector<double> msrJnt;
+		std::vector<JointState> msrJnt;
 		int nJoints;
 		std::vector<std::string> names;
 };
