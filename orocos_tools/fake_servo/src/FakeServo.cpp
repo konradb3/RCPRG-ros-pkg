2cc82243c55ba97472361e70073ff69b7bf1cf45
Konrad Banachowicz <konradb3@gmail.com>
2010-09-28 18:54:07 +0200

    fake_servo : added.

---
+++ orocos_tools/fake_servo/src/FakeServo.cpp
@@ -0,0 +1,66 @@
+/*
+ * FakeServo.cpp
+ *
+ *  Created on: 22-09-2010
+ *      Author: Konrad Banachowicz
+ */
+
+#include <ocl/Component.hpp>
+
+#include "FakeServo.h"
+
+FakeServo::FakeServo(const std::string& name) :
+    RTT::TaskContext(name), setpoint_port("setpoint"), jointState_port(
+      "jointState"), numberOfJoints_prop("numberOfJoints", "", 0)
+{
+
+  this->ports()->addPort(setpoint_port);
+  this->ports()->addPort(jointState_port);
+
+  this->addProperty(numberOfJoints_prop);
+}
+
+FakeServo::~FakeServo()
+{
+
+}
+
+bool FakeServo::configureHook()
+{
+  if ((numberOfJoints = numberOfJoints_prop.get()) == 0)
+    return false;
+  jointState.resize(numberOfJoints);
+  return true;
+}
+
+bool FakeServo::startHook()
+{
+
+  for (int i = 0; i < numberOfJoints; i++)
+  {
+    jointState[i].position = 0.0;
+    jointState[i].velocity = 0.0;
+    jointState[i].acceleration = 0.0;
+  }
+  return true;
+}
+
+void FakeServo::updateHook()
+{
+  if (setpoint_port.read(setpoint) == RTT::NewData)
+  {
+    if (setpoint.size() == numberOfJoints)
+    {
+      for (int i = 0; i < numberOfJoints; i++)
+      {
+        jointState[i].position = setpoint[i].position;
+        jointState[i].velocity = setpoint[i].velocity;
+        jointState[i].acceleration = setpoint[i].acceleration;
+
+      }
+    }
+  }
+  jointState_port.write(jointState);
+}
+
+ORO_CREATE_COMPONENT( FakeServo )
---
+++ orocos_tools/fake_servo/src/FakeServo.h
@@ -0,0 +1,40 @@
+/*
+ * FakeServo.h
+ *
+ *  Created on: 22-09-2010
+ *      Author: konrad
+ */
+
+#ifndef FAKESERVO_H_
+#define FAKESERVO_H_
+
+#include <string>
+#include <vector>
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Property.hpp>
+
+#include "Setpoint.hpp"
+#include "JointState.hpp"
+
+class FakeServo: public RTT::TaskContext {
+public:
+	FakeServo(const std::string& name);
+	virtual ~FakeServo();
+
+	bool startHook();
+	void updateHook();
+protected:
+	RTT::InputPort<std::vector<Setpoint> > setpoint_port;
+	RTT::OutputPort<std::vector<JointState> > jointState_port;
+
+	RTT::Property<int> numberOfJoints_prop;
+private:
+	std::vector<Setpoint> setpoint;
+	std::vector<JointState> jointState;
+
+	int numberOfJoints;
+};
+
+#endif /* FAKESERVO_H_ */
